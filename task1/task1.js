18700359a153222465a900859d472175a8697cae
heatclift77 <adityapratamaputra74@gmail.com>
2021-02-17 10:18:39 +0700

    first commit

---
+++ task1/task1.js
@@ -0,0 +1,49 @@
+// task 1
+
+//  no 1
+let skills = ["html", "css", "javascript", "reactjs", "bootstrap"]
+
+// method 1 toString()
+// console.log(skills.toString());
+//convert to string 
+
+//method 2 join()
+// console.log(skills.join(" "));
+// convert to string and defines the separator as param
+
+// method 3  pop()
+// let lastValue = skills.pop()
+// console.log(lastValue);
+// remove last value
+
+// method 4 shift()
+// console.log(skills.shift());
+// remove first value
+
+// method 5 unshift()
+// console.log(skills.unshift());
+//  add new value in array at the beginning and return new array length
+
+// method 6 splice()
+// console.log(skills.splice(2, 0, "reactnative", "kotlin"));
+// add new values to an array
+
+// method 7 concat()
+// console.log(skills.concat(["react native", "kotlin"]));
+// merged array and return new array
+
+// method 8 slice()
+// console.log(skills.slice(1));
+//  cut array and return new array
+
+// method 9 reverse()
+// console.log(skills.reverse());
+// reverse the items in array not return new array
+
+// method 10 push() 
+// console.log(skills.push("materiallize"));
+// add item in array at the end and return length of an array 
+
+// -----------------------------------------------------------
+console.log(skills);
+
